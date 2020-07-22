5eea86fcd3612147511de1d9d8d3da630f06f65b
Скоробагатько Иван <skoriy@MacBook-Pro.local>
2020-07-22 18:13:21 +0300

    mac commit

--- terraform/environment.tf
+++ terraform/environment.tf
@@ -224,6 +224,7 @@ resource "aws_instance" "DevTools" {
       "sudo apt-get install --yes docker.io",
       "sudo systemctl start docker",
       "sudo systemctl enable docker",
+      "sudo usermod -aG docker $USER",
       # "echo --------------INSTALLING DOCKER-REGISTRY-------------",
       # "sudo curl -L 'https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose",
       # "sudo chmod +x /usr/local/bin/docker-compose",
